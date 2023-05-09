4dafc42ede5fb0bb9968bcddb2d24645b09ec569
Arooj Chaudhry <s204759@student.dtu.dk>
2023-05-09 16:22:54 +0200

    added new api key and avacadofood as the fetching doc in recipes

---
+++ food_wise/ios/foodwise/foodwise/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  foodwise
+//
+//  Created by Arooj Chaudhry on 5/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ food_wise/ios/foodwise/foodwise/foodwiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  foodwiseApp.swift
+//  foodwise
+//
+//  Created by Arooj Chaudhry on 5/9/23.
+//
+
+import SwiftUI
+
+@main
+struct foodwiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
